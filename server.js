217ed7451e19c9f36dab321b63a4897ca9821db7
Aaron <a.rcottrill521@gmail.com>
2021-02-25 12:49:18 -0500

    Start adding sessions

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
+const passport = require("passport");
+const session = require("express-session");
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
@@ -11,10 +13,21 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+app.use(passport.initialize());
+
 app.set('view engine', 'pug');
 
+app.use(passport.session());
+
 app.route('/').get((req, res) => {
-  res.render(process.cwd() + '/views/pug');
+  res.render(process.cwd() + '/views/pug', { title: 'Hello', message: 'Please login' });
 });
 
 const PORT = process.env.PORT || 3000;
